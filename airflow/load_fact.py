45dee4143dc02f0b93b17f3ada04c631a40c108c
Salvatore Architetto <sal.architetto862@gmail.com>
2019-09-17 10:33:14 -0400

    Update load_fact.py

--- airflow/load_fact.py
+++ airflow/load_fact.py
@@ -2,40 +2,35 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from helpers import SqlQueries
 import logging
 
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
-    
-    sql_insert = """ 
-    INSERT INTO {}
-    {};
-    COMMIT;
+    insert_into_stmt = """
+        INSERT INTO {table} 
+        {select_query}
     """
-    
+
     @apply_defaults
     def __init__(self,
-
-                 redshift_conn_id = '',
-                 aws_credentials_id = '',
-                 table = '',
-                 query = '',
+                 redshift_conn_id,
+                 table,
+                 select_query,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id,
-#         self.aws_credentials_id = aws_credentials_id,
-        self.table = table,
-        self.query = query,
+
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_query = select_query
 
     def execute(self, context):
-        redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
-        self.log.info('Loading information into the fact table')
-        formatted_sql = LoadFactOperator.sql_insert.format(
-            self.table,
-            self.query
-        )
-        self.log.info(formatted_sql)
-        redshift.run(str(formatted_sql))
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        redshift.run(LoadFactOperator.insert_into_stmt.format(
+            table=self.table,
+            select_query=self.select_query
+        ))
         
